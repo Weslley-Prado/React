ee5379c88cb24316b15fe1d79f26f9069c91f72d
Weslley Prado <weslley.prado73@gmail.com>
2022-02-24 15:09:34 -0300

    Learn React

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//To render in the page, you need to write the function below
+
+ReactDOM.render(<h1>React is JavaScript Library </h1>, document.getElementById('root'))
