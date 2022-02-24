6b9f8a2be4e12e2b66c0736916de038689994437
Weslley Prado <weslley.prado73@gmail.com>
2022-02-24 15:21:19 -0300

    Learn React

--- src/index.js
+++ src/index.js
@@ -6,6 +6,5 @@ import ReactDOM from "react-dom";
 ReactDOM.render(<h1>React is JavaScript Library </h1>, document.getElementById('root'))
 
 //Or you could write 
-
 const element = <h1>I like React </h1>
 ReactDOM.render(element, document.getElementById('root'))
