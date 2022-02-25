0ae01b619cbf291aaef497a53a97d0f749ae74ea
Weslley Prado <weslley.prado73@gmail.com>
2022-02-25 14:00:50 -0300

    componentes

---
+++ src/componentes/component.jsx
@@ -0,0 +1,3 @@
+export default function(){
+    return <h1>I'm sending this content </h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import FirstComponent from './componentes/component'
 
 //To render in the page, you need to write the function below
 
@@ -8,3 +9,19 @@ ReactDOM.render(<h1>React is JavaScript Library </h1>, document.getElementById('
 //Or you could write 
 const element = <h1>I like React </h1>
 ReactDOM.render(element, document.getElementById('root'))
+
+/*
+ A important principle is to use component for our application.
+ For example:
+*/
+
+ReactDOM.render(<FirstComponent/>, document.getElementById('root'))
+/*
+ In this example, we must to notice that it was acessed the folders:
+ -componentes
+     - component.jsx
+ How? In this index.js, we import FirstComponent from this folder and 
+ we render with tag <FirstComponent/>
+*/
+
+
