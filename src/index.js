e90ef93f7fa4aadfabf1c85b5d14358ff3ced297
Weslley Prado <weslley.prado73@gmail.com>
2022-02-25 14:01:23 -0300

    componentes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import FirstComponent from './componentes/component'
 
-//To render in the page, you need to write the function below
-
+/*
+ To render in the page, you need to write the function below
+*/
 ReactDOM.render(<h1>React is JavaScript Library </h1>, document.getElementById('root'))
 
-//Or you could write 
+/*
+ Or you could write 
+*/
 const element = <h1>I like React </h1>
 ReactDOM.render(element, document.getElementById('root'))
 
@@ -14,7 +17,6 @@ ReactDOM.render(element, document.getElementById('root'))
  A important principle is to use component for our application.
  For example:
 */
-
 ReactDOM.render(<FirstComponent/>, document.getElementById('root'))
 /*
  In this example, we must to notice that it was acessed the folders:
